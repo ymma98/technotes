754358ea55a73b61df42bc4fff903212eb1de142
garnet <53188889+ymma98@users.noreply.github.com>
2024-11-06 10:46:58 +0800

    Update mathjax.js

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,9 +1,35 @@
+/*
+ref: https://squidfunk.github.io/mkdocs-material/reference/math/
+https://docs.mathjax.org/en/latest/input/tex/extensions/mathtools.html
+*/
 window.MathJax = {
+  loader: {load: ['[tex]/mathtools']},
   tex: {
     inlineMath: [["\\(", "\\)"]],
     displayMath: [["\\[", "\\]"]],
     processEscapes: true,
-    processEnvironments: true
+    processEnvironments: true,
+    mathtools: {
+      multlinegap: '1em',
+      multlined-pos: 'c',
+      firstline-afterskip: '',
+      lastline-preskip: '',
+      smallmatrix-align: 'c',
+      shortvdotsadjustabove: '.2em',
+      shortvdotsadjustbelow: '.2em',
+      centercolon: false,
+      centercolon-offset: '.04em',
+      thincolon-dx: '-.04em',
+      thincolon-dw: '-.08em',
+      use-unicode: false,
+      prescript-sub-format: '',
+      prescript-sup-format: '',
+      prescript-arg-format: '',
+      allow-mathtoolsset: true,
+      pairedDelimiters: {},
+      tagforms: {}
+    },
+    packages: {'[+]': ['mathtools']},
   },
   options: {
     ignoreHtmlClass: ".*|",
